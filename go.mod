68e6276bfc7da79bfa7b4f9e14fe7da44259e351
Jeff Anderson <jeff@jeff-anderson.com>
2022-04-11 23:11:29 -0400

    Add H3 Geo and accept command line parms

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module web-client
 
 go 1.18
+
+require github.com/uber/h3-go v3.0.1+incompatible // indirect
--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"fmt"
+	"github.com/uber/h3-go"
+	"math/rand"
+	"time"
 	"web-client/astra"
 )
 
+var defaultResolution = flag.Int("resolution", 1, "the H3 resolution to search")
+var sampleSize = flag.Int("sampleSize", 100, "the number of samples to test")
+
 func main() {
-	records, err := astra.GetForecastForLocation("834c1dfffffffff", "20020506")
-	if err != nil {
-		panic(err)
+	flag.Parse()
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	var ttlTime time.Duration
+
+	for i := 0; i < *sampleSize; i++ {
+		lat := (r.Float64() * 180) - 90
+		long := (r.Float64() * 360) - 180
+		point := h3.FromGeo(h3.GeoCoord{Latitude: lat, Longitude: long}, *defaultResolution)
+		// fmt.Printf("%f, %f index: %#v is valid: %v\n, ", lat, long, point, h3.IsValid(point))
+		start := time.Now()
+		loc, _ := astra.GetForecastForLocation(h3.ToString(point), "20020506")
+		end := time.Now()
+		ttlTime = ttlTime + end.Sub(start)
+		if loc == nil {
+			fmt.Printf("No match found for %#v\n", point)
+		}
 	}
-	for i := 0; i < len(records); i++ {
-		log.Printf("Record: %#v\n", records[i])
-	}
+
+	fmt.Printf("Found %d matches in %s. Average time %s\n", *sampleSize, ttlTime, ttlTime/time.Duration(*sampleSize))
 
 }
